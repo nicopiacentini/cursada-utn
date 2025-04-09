de0af96d62f62b20e051ffe606255bd0840de7fc
nicopiacentini <npiacentini@frba.utn.edu.ar>
2025-04-09 19:44:47 -0300

    java

--- diseno-de-sistemas/practicaJava/untitled/src/main/java/domain/lugares/Ciudad.java
+++ diseno-de-sistemas/practicaJava/untitled/src/main/java/domain/lugares/Ciudad.java
@@ -1,6 +1,9 @@
 package domain.lugares;
 
+import lombok.Getter;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ciudad {
@@ -8,11 +11,35 @@ public class Ciudad {
     private Pais pais;
     private List<Aeropuerto> aeropuertos;
 
+    public Ciudad(String nombre){
+        this.nombre = nombre;
+        this.aeropuertos = new ArrayList<>();
+    }
+
+
     public int cantidadDeAeropuertos(){
         return aeropuertos.size();
     }
 
-    public int cantidadDepPasajerosQueLlegaronElDia(LocalDate dia){
+    public int cantidadDepPasajerosQueLlegaronElDia(LocalDate dia) {
         return aeropuertos.stream().mapToInt(aeropuerto -> aeropuerto.cantidadDePasajerosQueLlegaronElDia(dia)).sum();
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
 }
+//TODO
